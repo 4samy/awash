a09b47ee88d26ae5eeece0533c41f269b99cebe0
4samy <2mfsamy@gmail.com>
2023-11-12 09:03:59 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,30 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 env.read_envfile()
 
-DEBUG = env("DEBUG", default="no", cast=bool)
-SECRET_KEY = env("SECRET_KEY", default="secret_key")
-
-# JWT signature configuration ***Salt is always required***
-TOKEN_TYPES = {
-    "session": {
-        "expires_in": 15552000,
-        "salt": "session"
-    }
-}
-
-
 ENABLED_MODULES = [
-    "auth",
-    "driver",
-    "restaurant",
-    "food_request",
-    "rewards",
-    "test",
 
-    "api"  # This always has to be last
+    "api" # This always has to be last
 ]
 
-SQLALCHEMY_DATABASE_URI = env("DATABASE_URL", default="postgresql:///DonateFood-API")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
+SQLALCHEMY_DATABASE_URI = env("DATABASE_URL", default="postgresql:///api")
