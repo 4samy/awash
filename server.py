d0ad2e04e4cb7fd42ff08b2b82a988c6044cd236
4samy <2mfsamy@gmail.com>
2023-11-12 09:10:12 +0400

    Add files via upload

--- server.py
+++ server.py
@@ -9,3 +9,4 @@ app = create_app()
 
 if __name__ == '__main__':
     socketio.run(app)
+
--- settings.py
+++ settings.py
@@ -6,9 +6,30 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 env.read_envfile()
 
-ENABLED_MODULES = [
+DEBUG = env("DEBUG", default="no", cast=bool)
+SECRET_KEY = env("SECRET_KEY", default="secret_key")
 
-    "api" # This always has to be last
+# JWT signature configuration ***Salt is always required***
+TOKEN_TYPES = {
+    "session": {
+        "expires_in": 15552000,
+        "salt": "session"
+    }
+}
+
+
+ENABLED_MODULES = [
+    "auth",
+    "driver",
+    "restaurant",
+    "food_request",
+    "rewards",
+    "test",
+
+    "api"  # This always has to be last
 ]
 
-SQLALCHEMY_DATABASE_URI = env("DATABASE_URL", default="postgresql:///api")
+SQLALCHEMY_DATABASE_URI = env("DATABASE_URL", default="postgresql:///DonateFood-API")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
